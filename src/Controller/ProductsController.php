5e565b8f884562b8d765ed83dfc1e425cb0f0bd1
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2018-07-02 19:00:44 +0200

    added Product entity

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,13 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProductsController extends Controller
 {
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+
+    /**
+     * ProductsController constructor.
+     * @param ProductRepository $productRepository
+     */
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+
     public function index()
     {
-        return $this->render('products/index.html.twig');
+        $products = $this->productRepository->findAll();
+
+        return $this->render('products/index.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * @ParamConverter("post", class="App\Entity\Product")
+     */
+    public function show(Product $product)
+    {
+        return $this->render('products/show.html.twig', ['product' => $product]);
     }
 }
