1570c3704d2b0711a9fee06d707c1dee2a59c5ee
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2018-07-19 00:02:05 +0200

    added quantity field to product list in cart

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -3,9 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class OrdersController extends Controller
 {
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     public function list()
     {
         $orders = $this->getUser()->getOrders();
